a626d42cd97023a5bd966e28d91209a6bdb84a92
Kim Jun Ho <rlawnsgh933@naver.com>
2022-10-02 17:53:16 +0900

    테스트 추가

---
+++ src/views/InterFaceTest.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home" style="display: flex; justify-content: center">
+    <div style="display: flex; align-items: center">
+      {{ book.title }} <br />
+      {{ book.author }} <br />
+      {{ book.year }}
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue"
+
+interface Book {
+  title: string
+  author: string
+  year: number
+}
+
+export default defineComponent({
+  name: "InterFaceTest",
+  data() {
+    return {
+      book: {
+        title: "TypeScript interface Guide",
+        author: "team",
+        year: 2022
+      } as Book
+    }
+  }
+})
+</script>
+<style lang="scss" scoped></style>
---
+++ src/views/TsTest1.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home" style="display: flex; justify-content: center">
+    <div style="display: flex; align-items: center">
+      <button @click="subtract">-</button>
+      <div>{{ count }}</div>
+      <button @click="add">+</button>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: "TsTest1",
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    subtract() {
+      this.count -= 1
+    },
+    add() {
+      this.count += 1
+    }
+  }
+})
+</script>
+<style lang="scss" scoped></style>
